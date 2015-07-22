695defe4ec8e85443547f9b8b809c85c103ef106
Lijin <lijin@baabte.com>
2015-07-22 15:25:13 +0530

    Init

---
+++ candidate/99lmsCandidate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/lintuism/gitrepo/99lmsMobile/candidate/99lmsCandidate/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/lintuism/gitrepo/99lmsMobile/candidate/99lmsCandidate/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ candidate/99lmsCandidate/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/lintuism/gitrepo/99lmsMobile/candidate/99lmsCandidate/platforms/android/ant-build/AndroidManifest.xml : \
+/home/lintuism/gitrepo/99lmsMobile/candidate/99lmsCandidate/platforms/android/AndroidManifest.xml \
